e7aa7a8dbdf760f1b698c74f358a39a1c4067477
UduthaManasa <129256614+UduthaManasa@users.noreply.github.com>
2023-04-29 18:05:16 +0530

    Add files via upload
    project

---
+++ flights.php
@@ -0,0 +1,102 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>AirLines</title>
+  </head>
+  <body>
+    <center><p class="h1 text-danger"><b>AirLines</b></h1></center>
+    <div class="container-fluid">
+      <div class="row">
+        <!-- IndiGo -->
+          <div class="col-md-4">
+              <div class="card"style="width:30rem;">
+                  <img src="https://images.cnbctv18.com/wp-content/uploads/2022/02/IndiGo.jpg?im=FitAndFill,width=1200,height=900" class="card-in=mg-top"alt="indigo" style="height:250px;">
+                  <div class="card-body">
+                    <center><h5 class="h2 card-title"style="color:blue">IndiGo</h5></center>
+                  <center><a href=""class="btn btn-primary">Tickets booking</a></center>
+                  </div>
+              </div>
+          </div>
+          <br>
+          <br>
+          <!-- Air India -->
+          <div class="col-md-4">
+            <div class="card"style="width:30rem;">
+                <img src="https://img.etimg.com/thumb/msid-96867568,width-1070,height-580,imgsize-1223236,overlay-economictimes/photo.jpg" class="card-in=mg-top"alt="air india" style="height: 250px;">
+                <div class="card-body">
+                  <center><h5 class=" h2 card-title"style="color:blue">Air India</h5></center>
+                    
+                    <center><a href=""class="btn btn-primary">Tickets booking</a></center>
+                </div>
+            </div>
+        </div>
+        <br>
+          <br>
+        <div class="col-md-4">
+          <div class="card"style="width:30rem;">
+              <img src="https://s28477.pcdn.co/wp-content/uploads/2020/06/AirAsia_India_3.jpg" class="card-in=mg-top"alt="AirAsia" style="height: 250px;">
+              <div class="card-body">
+                <center><h5 class="h2 card-title"style="color:blue">AirAsia</h5></center>
+                  
+                  <center> <a href=""class="btn btn-primary">Tickets booking</a></center>
+              </div>
+          </div>
+      </div>
+      <br>
+          <br>
+          <div class="col-md-4">
+            <div class="card"style="width:30rem;">
+                <img src="https://www.deccanherald.com/sites/dh/files/articleimages/2023/01/09/screenshot-2023-01-09-212011-1179372-1673279756.jpg" class="card-in=mg-top"alt="go first" style="height: 250px;">
+                <div class="card-body">
+                  <center> <h5 class="h2 card-title"style="color:blue">Go First</h5></center>
+                    
+                    <center> <a href=""class="btn btn-primary">Tickets booking</a></center>
+                </div>
+            </div>
+        </div>
+        <br>
+            <br>
+            <div class="col-md-4">
+              <div class="card"style="width:30rem;">
+                  <img src="https://static.theprint.in/wp-content/uploads/2019/03/maxresdefault.jpg?compress=true&quality=80&w=376&dpr=2.6" class="card-in=mg-top"alt="SpiceJet" style="height: 250px;">
+                  <div class="card-body">
+                    <center> <h5 class="h2 card-title"style="color:blue">SpiceJet</h5></center>
+                      
+                      <center> <a href=""class="btn btn-primary">Tickets bookin</a></center>
+                  </div>
+              </div>
+          </div>
+          <br>
+              <br>
+              <div class="col-md-4">
+                <div class="card"style="width:30rem;">
+                    <img src="https://th-i.thgim.com/public/incoming/68qy1m/article66719758.ece/alternates/FREE_1200/80725_24_8_2022_13_57_57_1_MYSURUTOCHENNAIFLIGHT.JPG" class="card-in=mg-top"alt="Alliance Air" style="height: 250px;">
+                    <div class="card-body">
+                      <center> <h5 class="h2 card-title"style="color:blue">Alliance Air</h5></center>
+                        
+                        <center> <a href=""class="btn btn-primary">Tickets booking</a></center>
+                    </div>
+                </div>
+            </div>
+            <br>
+                <br>
+      
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
