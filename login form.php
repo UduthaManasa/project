8cf8a809c97ab05cbb459882ba2faf185207c57f
UduthaManasa <129256614+UduthaManasa@users.noreply.github.com>
2023-04-29 17:14:24 +0530

    Add files via upload
    php

---
+++ login form.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Login form</title>
+    <style>
+        body{
+            background-image: url("https://imgeng.jagran.com/images/2022/dec/TRIP1670589457397.jpg");
+        background-repeat: no-repeat;
+        background-size: cover;
+        }
+    </style>
+  </head>
+  <body>
+    <br><br>
+    <center><h1><b>Login </b></h1>
+        <br><br>
+        <form>
+    <label for=""><b>Email ID</b></label>
+    <input type="text" name="Email" id="" place holder="enter Email ID"><br><br>
+    <label for="p1"><b>Password</b></label>
+        <input type="password"name="word"id="p1" place holder="enter password"><br><br>
+        <label for="p1">  <b>Confirm password</b></label>
+        <input type="password"name="word"id="p1" place holder="enter Confirm password"><br><br>
+       
+        <a href="Php Task Travel1.php"><input type="button"class="btn btn-dark"style="background-color:black"  value="login"/></a>&nbsp &nbsp &nbsp
+        <a href="forgot password.php">forgot password</a>
+        <br>
+    
+        <a href="registration form.php">Sign Up</a> 
+    </form></center>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
